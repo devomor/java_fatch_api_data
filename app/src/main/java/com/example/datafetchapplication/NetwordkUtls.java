e2575947f3d6654ece26b64d5e058f7a9016212f
devomor <mohammadomar01312@gmail.com>
2025-02-20 15:08:27 +0600

    update code

--- app/src/main/java/com/example/datafetchapplication/NetwordkUtls.java
+++ app/src/main/java/com/example/datafetchapplication/NetwordkUtls.java
@@ -8,8 +8,10 @@ import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 import android.content.Context;
+import org.json.JSONObject;
 
 public class NetwordkUtls {
 
@@ -17,6 +19,69 @@ public class NetwordkUtls {
         RequestQueue queue = Volley.newRequestQueue(context);
 
         StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        callback.onSuccess(response);
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        callback.onError(error.toString());
+                    }
+                });
+
+        queue.add(stringRequest);
+    }
+
+    // Method to send data (POST)
+    public static void postData(Context context, String url, JSONObject postData, final VolleyCallback callback) {
+        RequestQueue queue = Volley.newRequestQueue(context);
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, url, postData,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        callback.onSuccess(response.toString());
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        callback.onError(error.toString());
+                    }
+                });
+
+        queue.add(jsonObjectRequest);
+    }
+
+    // Method to update data (PUT)
+    public static void putData(Context context, String url, JSONObject putData, final VolleyCallback callback) {
+        RequestQueue queue = Volley.newRequestQueue(context);
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.PUT, url, putData,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        callback.onSuccess(response.toString());
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        callback.onError(error.toString());
+                    }
+                });
+
+        queue.add(jsonObjectRequest);
+    }
+
+    // Method to delete data (DELETE)
+    public static void deleteData(Context context, String url, final VolleyCallback callback) {
+        RequestQueue queue = Volley.newRequestQueue(context);
+
+        StringRequest stringRequest = new StringRequest(Request.Method.DELETE, url,
                 new Response.Listener<String>() {
                     @Override
                     public void onResponse(String response) {
